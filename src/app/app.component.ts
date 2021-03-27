7354b118308d8c541cc74d6ade6d03139d18be93
Akhlaque <akhlaquea01@gmail.com>
2021-03-27 19:10:53 +0530

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {FormControl,FormGroup, Validators }from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'form';
+  loginForm = new FormGroup({
+    email:new FormControl("atts@gmail.com",Validators.required),
+    password:new FormControl("")
+  })
+  onSubmit(data) {
+    console.log(data)
+  }
+  get email() { return this.loginForm.get('email') }
+  userData = {
+    email:"akhlaque@gmail.com"
+  }
+}
